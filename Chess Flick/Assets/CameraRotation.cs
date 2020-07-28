8401386f1c71bcff29e4a130554a051847ccd72a
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-07-28 20:40:35 +0530

    Camera Rotation done

--- Chess Flick/Assets/CameraRotation.cs
+++ Chess Flick/Assets/CameraRotation.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    private float speed = 1f;
-    float minXRot = 42.5f;
-    float maxXRot = 60.0f;
-    private Quaternion camRot;
+   private Touch initTouch = new Touch();
+   private float rotX = 0f;
+   private float rotY = 0f;
+   private Vector3 origRot;
 
-    void Start()
-    {
-        camRot = transform.localRotation;
-    }
-    void FixedUpdate()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-        camRot.x += Input.GetAxis("Mouse Y") * speed;
-        camRot.y += Input.GetAxis("Mouse X") * speed;
+   public float rotSpeed = 0.5f;
+   private float dir = -1;
 
-        transform.localRotation = Quaternion.Euler(camRot.x, camRot.y, 0);
-        }
-    }
+   void Start()
+   {
+       origRot = transform.eulerAngles;
+       rotX = origRot.x;
+       rotY = origRot.y;
+   }
+
+   void FixedUpdate()
+   {
+       foreach(Touch touch in Input.touches)
+       {
+           if(touch.phase == TouchPhase.Began)
+           {
+               initTouch = touch;
+           }
+           else if(touch.phase == TouchPhase.Moved)
+           {
+               float deltaX = initTouch.position.x - touch.position.x;
+               float deltaY = initTouch.position.y - touch.position.y;
+               rotX -= deltaY * Time.deltaTime * rotSpeed * dir;
+               rotY -= deltaX * Time.deltaTime * rotSpeed * dir;
+               rotX = Mathf.Clamp(rotX, 40f, 60f);
+               //rotY = Mathf.Clamp(rotY, 0f, 180f);
+               transform.eulerAngles = new Vector3(rotX, 0, 0f);
+           }
+           else if(touch.phase == TouchPhase.Ended)
+           {
+               initTouch = new Touch();
+           }
+       }
+   }
 }
